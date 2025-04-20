d3bdd04c00e029b165a76eb745c8925e4505574e
Mitchell <mitchellthanath@gmail.com>
2025-04-20 12:22:18 -0400

    Vercel Build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/PortfolioWebsite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/PortfolioWebsite/' : '',
+  }
 }
 
 module.exports = nextConfig 
